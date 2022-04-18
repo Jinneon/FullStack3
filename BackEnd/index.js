f13dd209732d8d8802cc225ee8056f5287178f28
Jinneon <Jinneon@users.noreply.github.com>
2022-04-18 14:06:49 +0300

    make it better

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,7 +8,7 @@ app.use(cors())
 //app.use(morgan)
 //app.use(morgan("tiny"));
 morgan.token('body', (req) => JSON.stringify(req.body))
-//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body' ))
+//app.use(morgan(':metahod :url :status :res[content-length] - :response-time ms :body' ))
 let persons = [
   {
     name: 'Arto Hellas',
@@ -136,123 +136,7 @@ app.listen(PORT, () => {
 })
 
 
-//In the first row, the application imports Node's built-in web server module. 
-//This is practically what we have already been doing in our browser-side code
-//, but with a slightly different syntax:
-//const http = require('http')
-/*const express = require('express')
-const app = express()
-app.use(express.json())*/
-//const app = express()
-//Next, we define two routes to the application.
-// The first one defines an event handler that is used to handle 
-//HTTP GET requests made to the application's / root:
 
-//The event handler function accepts two parameters.
-  // The first request parameter contains all of the information of the HTTP request,
-  //  second response parameter is used to define how the request is responded to.
-/*app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })*/
-  //The second route defines an event handler that handles HTTP GET requests 
-  //made to the notes path of the application:
-  /*app.get('/api/notes', (request, response) => {
-    response.json(notes)
-  })*/
-  //new
-  /*let notes = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-  
- 
-  
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
-  
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
-  
-  app.get('/api/notes', (req, res) => {
-    res.json(notes)
-  })
-  
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
-  
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-  
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  
-  const PORT = 3002
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  }
-  )
-
-/*const app = http.createServer((request, response) => { 
-     response.writeHead(200, { 'Content-Type': 'application/json' }) 
- response.end(JSON.stringify(notes))})
-//Let's restart the server
-// (you can shut the server down by typing Ctrl+C in the console)
-const PORT = 3002
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)*/
 //npm install --save-dev nodemon to automatically refresh page
 // We can start our application with nodemon like this:
 // node_modules/.bin/nodemon index.js
