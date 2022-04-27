dba879ad5cb03eea59e93b8d4eb36f933a923316
Jinneon <Jinneon@users.noreply.github.com>
2022-04-27 18:33:02 +0300

    Fourth try

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ app.listen(ORT, () => {
 }) */
 
 //const PORT = process.env.PORT || 3001
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)  
 })
